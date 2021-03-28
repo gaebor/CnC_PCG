4c0e62eb9266e2caf278c0ca14ee6979c7f760ef
Gabor Borbely <gaebor@gmail.com>
2021-03-28 12:40:04 +0200

    pytest for tiles file

---
+++ test_tiles.py
@@ -0,0 +1,11 @@
+import pytest
+
+import generate
+
+
+@pytest.mark.parametrize("filename", [('ra1_tiles.json')], ids=["RA1"])
+def test_tiles_json(filename):
+    for pattern, template, icon in generate.import_tiles_file(filename):
+        assert template.shape == icon.shape
+        assert template.ndim == 3 
+        assert pattern.ndim == 3
